7fd6e3f93e56c8eb5c35e48cf88bec637931d4ff
Willian Negishi <dakarakoso@Willians-MacBook-Pro.local>
2021-11-10 02:04:50 +0900

    building components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import "./App.css";
+import Particles from "react-particles-js";
 import Navigation from "./components/navigation/navigation";
 import Logo from "./components/logo/logo";
+import ImageLinkForm from "./components/imageLinkForm/imageLinkForm";
+import Rank from "./components/rank/rank";
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 150,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+  },
+};
 
 function App() {
   return (
     <div className="App">
+      <Particles className="particles" params={particlesOptions} />
       <Navigation />
       <Logo />
-      {/* {
+      <Rank />
       <ImageLinkForm />
+      {/* {
+      
       <FaceRecognition />} */}
     </div>
   );
--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Tilt from "react-tilt";
 import "./logo.css";
+import brain from "./brain2.png";
 const Logo = () => {
   return (
     <div className="ma4 mt0">
@@ -9,7 +10,9 @@ const Logo = () => {
         options={{ max: 25 }}
         style={{ height: 150, width: 150 }}
       >
-        <div className="Tilt-inner"> 👽 </div>
+        <div className="Tilt-inner pa3">
+          <img alt="logo" style={{ paddingTop: "16px" }} src={brain} />
+        </div>
       </Tilt>
     </div>
   );
